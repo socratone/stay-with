961da24b411a4c450b033277611973030c1b6764
Kiwon Kim <kiwon@elicer.com>
2022-08-22 01:25:42 +0900

    feat: add dark paper palette

---
+++ contexts/ThemeProvider.tsx
@@ -0,0 +1,41 @@
+import {
+  ThemeProvider as MuiThemeProvider,
+  useMediaQuery,
+} from '@mui/material';
+
+import { createTheme } from '@mui/material';
+import { useMemo } from 'react';
+
+// https://stackoverflow.com/questions/60424596/cant-customize-color-palette-types-on-material-ui-theme-in-typescript
+declare module '@mui/material/styles/createPalette' {
+  interface Palette {
+    paper?: Palette['primary'];
+  }
+  interface PaletteOptions {
+    paper?: PaletteOptions['primary'];
+  }
+}
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          paper: {
+            main: prefersDarkMode ? 'rgb(55 40 14)' : 'rgb(255, 249, 238)',
+          },
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+};
+
+export default ThemeProvider;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material';
-import theme from '../configs/theme';
-
 // swiper
 import 'swiper/swiper.scss';
 
+import type { AppProps } from 'next/app';
+import ThemeProvider from '../contexts/ThemeProvider';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
   );
