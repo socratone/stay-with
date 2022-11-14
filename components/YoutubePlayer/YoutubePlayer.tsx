4d0eb711c287f62bab4798b7026d2412a2d73d51
Kiwon Kim <kiwon@elicer.com>
2022-11-15 00:58:47 +0900

    feat: 비디오 숨기기 기능 추가

--- components/YoutubePlayer/YoutubePlayer.tsx
+++ components/YoutubePlayer/YoutubePlayer.tsx
@@ -1,31 +1,62 @@
-import { Box, useTheme } from '@mui/material';
+import { Box, IconButton, useTheme } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useState } from 'react';
 
 interface YoutubePlayerProps {
   videoId: string;
 }
 
+const VIDEO_HEIGHT = 120;
+
 const YoutubePlayer: React.FC<YoutubePlayerProps> = ({ videoId }) => {
   const theme = useTheme();
+  const [hidden, setHidden] = useState(false);
+
+  const handleClick = () => {
+    setHidden((hidden) => !hidden);
+  };
 
   return (
     <Box
       sx={{
-        aspectRatio: '1920 / 1080',
-        height: 120,
         position: 'fixed',
         left: theme.spacing(2),
         bottom: theme.spacing(2),
-        borderRadius: 3,
-        overflow: 'hidden',
+        transform: `translateY(${hidden ? `${VIDEO_HEIGHT}px` : 0})`,
+        transition: 'all 0.3s ease',
       }}
     >
-      <iframe
-        width="100%"
-        height="100%"
-        id="ytplayer"
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-        frameBorder="0"
-      />
+      <IconButton
+        size="small"
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          transform: 'translateY(-100%)',
+        }}
+        onClick={handleClick}
+      >
+        {hidden ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+      </IconButton>
+      <Box
+        sx={{
+          aspectRatio: '1920 / 1080',
+          height: VIDEO_HEIGHT,
+          borderRadius: 3,
+          overflow: 'hidden',
+          opacity: hidden ? 0 : 1,
+          transition: 'all 0.3s ease',
+        }}
+      >
+        <iframe
+          width="100%"
+          height="100%"
+          id="ytplayer"
+          src={`https://www.youtube.com/embed/${videoId}?color=white&fs=0`}
+          frameBorder="0"
+        />
+      </Box>
     </Box>
   );
 };
