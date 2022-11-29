175056a6466eea3835a47966dd3c1c73d49b2fba
Kiwon Kim <kiwon@elicer.com>
2022-11-29 21:56:58 +0900

    feat: 댓글에 쓴 사람 이름 추가

--- sections/CommentDrawer/CommentItem.tsx
+++ sections/CommentDrawer/CommentItem.tsx
@@ -22,20 +22,18 @@ const CommentItem: React.FC<CommentItemProps> = ({
       onClick={onClick}
       sx={{ bgcolor: isSelected ? theme.palette.primary.main : undefined }}
     >
-      <Box
-        display="flex"
-        alignItems="center"
-        gap={1}
-        px={2}
-        py={1}
-        width="100%"
-      >
+      <Box display="flex" gap={1} px={2} py={1} width="100%">
         {image ? (
           <Avatar alt="Profile" src={image} />
         ) : (
           <Avatar sx={{ width: 34, height: 34 }}>{name?.[0] ?? 'P'}</Avatar>
         )}
-        <Typography>{message}</Typography>
+        <Box>
+          <Typography align="left" fontWeight={600}>
+            {name}
+          </Typography>
+          <Typography align="left">{message}</Typography>
+        </Box>
       </Box>
     </ButtonBase>
   );
