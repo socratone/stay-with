5674a21ec9f19bb2f0c3d53984ba0090623b686c
Kiwon Kim <kiwon@elicer.com>
2023-03-03 23:00:38 +0900

    feat: add error handle in login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,15 +2,23 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import GlobalHeader from 'components/GlobalHeader';
 import Script from 'next/script';
+import { useSnackbar } from 'notistack';
 import { useRef } from 'react';
 
 const Login = () => {
+  const { enqueueSnackbar } = useSnackbar();
   const KakaoRef = useRef<any | null>(null);
 
-  const handleLoad = () => {
+  const handleScriptLoad = () => {
     const { Kakao }: any = window;
     Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY);
     KakaoRef.current = Kakao;
+  };
+
+  const handleScriptError = () => {
+    enqueueSnackbar('Kakao SDK를 불러오는 중에 에러가 발생했습니다.', {
+      variant: 'error',
+    });
   };
 
   const handleKakaoLoginClick = () => {
@@ -18,6 +26,13 @@ const Login = () => {
       KakaoRef.current.Auth.authorize({
         redirectUri: `${process.env.NEXT_PUBLIC_BASE_URL}/login/redirect`,
       });
+    } else {
+      enqueueSnackbar(
+        'Kakao SDK를 불러오는 중에 에러가 발생하여 로그인을 할 수 없습니다.',
+        {
+          variant: 'error',
+        }
+      );
     }
   };
 
@@ -44,7 +59,8 @@ const Login = () => {
         src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
         integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
         crossOrigin="anonymous"
-        onLoad={handleLoad}
+        onLoad={handleScriptLoad}
+        onError={handleScriptError}
       />
     </>
   );
