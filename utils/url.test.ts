8c4498fee5f7cbe6c5dc54117f192f0a08cabc27
Kiwon Kim <kiwon@elicer.com>
2023-05-07 22:24:24 +0900

    feat: add filterUrlQuery

--- utils/url.test.ts
+++ utils/url.test.ts
@@ -1,4 +1,4 @@
-import { getUrlPath } from './url';
+import { filterUrlQuery, getUrlPath } from './url';
 
 describe('getUrlPath', () => {
   it('should only return the URL path.', () => {
@@ -9,6 +9,23 @@ describe('getUrlPath', () => {
 
   it('should work even when there is no query.', () => {
     expect(getUrlPath('https://example.com/blog')).toBe(
+      'https://example.com/blog'
+    );
+  });
+});
+
+describe('filterUrlQuery', () => {
+  it('should delete the correct query.', () => {
+    expect(
+      filterUrlQuery(
+        'https://example.com/blog?count=10&offset=20&search=apple',
+        'offset'
+      )
+    ).toBe('https://example.com/blog?count=10&search=apple');
+  });
+
+  it('should work even when there is no query.', () => {
+    expect(filterUrlQuery('https://example.com/blog', 'offset')).toBe(
       'https://example.com/blog'
     );
   });
--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,19 @@
+import queryString from 'query-string';
+
 export const getUrlPath = (url: string) => {
   const [path] = url.split('?');
+  return path;
+};
+
+export const filterUrlQuery = (url: string, query: string) => {
+  const [path, rawQueryString] = url.split('?');
+  const parsedQuery = queryString.parse(rawQueryString);
+  delete parsedQuery[query];
+  const stiringifiedQuery = queryString.stringify(parsedQuery);
+
+  if (stiringifiedQuery) {
+    return `${path}?${stiringifiedQuery}`;
+  }
+
   return path;
 };
