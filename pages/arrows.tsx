418dae171952f3ce1e716ad13eca078418555ce3
Kiwon Kim <kiwon@elicer.com>
2023-05-14 23:43:05 +0900

    feat: add offset candle images

--- pages/arrows.tsx
+++ pages/arrows.tsx
@@ -26,21 +26,46 @@ const Arrows = () => {
 
   const bigRange = getAscendNumbers(100);
 
-  const drawStar = () => {
+  const getRandomCandleImageSrc = (index: number) => {
+    const order = index % 8;
+    switch (order) {
+      case 0:
+        return '/candle.gif';
+      case 1:
+        return '/candle-flip-offset-1.gif';
+      case 2:
+        return '/candle-offset-2.gif';
+      case 3:
+        return '/candle-flip-offset-3.gif';
+      case 4:
+        return '/candle-offset-4.gif';
+      case 5:
+        return '/candle-flip-offset-5.gif';
+      case 6:
+        return '/candle-offset-6.gif';
+      case 7:
+      default:
+        return '/candle-flip-offset-7.gif';
+    }
+  };
+
+  const drawStar = (index: number) => {
     const top = randomFrom(bigRange);
     const left = randomFrom(bigRange);
 
     if (divRef.current) {
       divRef.current.insertAdjacentHTML(
         'beforeend',
-        `<img src="/candle.gif" alt="small candle" class="candle" style="top: ${top}%; left: ${left}%;"></div>`
+        `<img src="${getRandomCandleImageSrc(
+          index
+        )}" alt="small candle" class="candle" style="top: ${top}%; left: ${left}%;"></div>`
       );
     }
   };
 
   useMount(() => {
     for (let i = 0; i < 100; i++) {
-      drawStar();
+      drawStar(i);
     }
   });
 
