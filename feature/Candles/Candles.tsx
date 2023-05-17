1be087fd2543e1f209e7e7f0fa5f65b455a00ea3
Kiwon Kim <kiwon@elicer.com>
2023-05-18 03:47:28 +0900

    feat: add api to arrows

---
+++ feature/Candles/Candles.tsx
@@ -0,0 +1,124 @@
+import { CircularProgress } from '@mui/material';
+import Box from '@mui/material/Box';
+import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
+import useArrows from 'hooks/api/useArrows';
+import { cloneDeep } from 'lodash';
+import { useEffect, useRef, useState } from 'react';
+import { Arrow, User } from 'types/document';
+import {
+  assignValue,
+  assignValues,
+  createEmptyBoard,
+  parseBoardToArrayWithCoordinate,
+} from 'utils/board';
+
+import CandleItem, { CANDLE_HEIGHT, CANDLE_WIDTH } from './CandleItem';
+
+const getRandomCandleImageSrc = (index: number) => {
+  const order = index % 8;
+  switch (order) {
+    case 0:
+      return '/candle.gif';
+    case 1:
+      return '/candle-flip-offset-1.gif';
+    case 2:
+      return '/candle-offset-2.gif';
+    case 3:
+      return '/candle-flip-offset-3.gif';
+    case 4:
+      return '/candle-offset-4.gif';
+    case 5:
+      return '/candle-flip-offset-5.gif';
+    case 6:
+      return '/candle-offset-6.gif';
+    case 7:
+    default:
+      return '/candle-flip-offset-7.gif';
+  }
+};
+
+export type Candle = Partial<Arrow> & {
+  user?: User;
+  createdAt?: Date;
+};
+
+type CandlesProps = {
+  additionalCandles: Candle[];
+};
+
+const Candles: React.FC<CandlesProps> = ({ additionalCandles }) => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const [board, setBoard] = useState<(Candle | null)[][]>([]);
+
+  const {
+    data: arrowsData,
+    isLoading: arrowsLoading,
+    isError: arrowsError,
+  } = useArrows({
+    skip: 0,
+    limit: 50, // TODO: 페이지네이션
+  });
+
+  const candles = arrowsData ? parseBoardToArrayWithCoordinate(board) : [];
+
+  // TODO: viewport가 변할 때마다 갱신
+
+  // data를 받아서 board에 넣는다.
+  useEffect(() => {
+    const screen = divRef.current;
+
+    if (screen && arrowsData) {
+      const screenWidth = screen.offsetWidth;
+      const screenHeight = screen.offsetHeight;
+      const columnColunt = Math.floor(screenWidth / CANDLE_WIDTH);
+      const rowColunt = Math.floor(screenHeight / CANDLE_HEIGHT);
+
+      const board = createEmptyBoard(rowColunt, columnColunt);
+      assignValues(board, arrowsData.arrows);
+      setBoard(board);
+    }
+  }, [arrowsData]);
+
+  // message를 입력하여 candle이 추가되면 board에 넣는다.
+  useEffect(() => {
+    if (additionalCandles.length > 0) {
+      const newAdditionalCandle =
+        additionalCandles[additionalCandles.length - 1];
+
+      setBoard((board) => {
+        const newBoard = cloneDeep(board);
+        assignValue(newBoard, newAdditionalCandle);
+        return newBoard;
+      });
+    }
+  }, [additionalCandles]);
+
+  return (
+    <Box
+      ref={divRef}
+      flexGrow={1}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ bgcolor: 'black', position: 'relative' }}
+    >
+      {arrowsLoading ? (
+        <CircularProgress />
+      ) : arrowsError ? (
+        <ErrorMessage />
+      ) : (
+        candles.map((candle, index) => (
+          <CandleItem
+            key={candle._id}
+            row={candle.row}
+            column={candle.column}
+            imageSrc={getRandomCandleImageSrc(index)}
+          />
+        ))
+      )}
+    </Box>
+  );
+};
+
+export default Candles;
--- pages/arrows.tsx
+++ pages/arrows.tsx
@@ -1,4 +1,3 @@
-import { styled } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
@@ -6,105 +5,55 @@ import TextField from '@mui/material/TextField';
 import GlobalHeader from 'components/GlobalHeader/GlobalHeader';
 import Meta from 'components/Meta/Meta';
 import ThemeProvider from 'contexts/ThemeProvider';
-import { useRef, useState } from 'react';
-import { useMount } from 'react-use';
-import {
-  addCoordinateFrom2DArray,
-  assignTruthyValues,
-  create2DArray,
-} from 'utils/array';
-
-// TODO: api 생성
-const MOCK_VALUES = [
-  { name: 'John', message: 'pray 1' },
-  { name: 'John', message: 'pray 2' },
-  { name: 'John', message: 'pray 3' },
-  { name: 'John', message: 'pray 4' },
-];
-
-const CANDLE_WIDTH = 10;
-const CANDLE_HEIGHT = 24;
-
-const StyledBox = styled(Box)`
-  .candle {
-    position: absolute;
-    width: ${CANDLE_WIDTH}px;
-    display: block;
-
-    :hover {
-      transform: scale(2);
-      transition: all 0.3s ease;
-    }
-  }
-`;
+import Candles from 'feature/Candles';
+import { Candle } from 'feature/Candles/Candles';
+import { postArrow } from 'helpers/axios';
+import useAuth from 'hooks/auth/useAuth';
+import { useRouter } from 'next/router';
+import { enqueueSnackbar } from 'notistack';
+import { useState } from 'react';
+import { useIntl } from 'react-intl';
+import { createUniqueId } from 'utils/id';
 
 const Arrows = () => {
-  const divRef = useRef<HTMLDivElement>(null);
+  const { formatMessage } = useIntl();
+  const router = useRouter();
+
+  const { user, logout } = useAuth();
 
   const [message, setMessage] = useState('');
-
-  const getRandomCandleImageSrc = (index: number) => {
-    const order = index % 8;
-    switch (order) {
-      case 0:
-        return '/candle.gif';
-      case 1:
-        return '/candle-flip-offset-1.gif';
-      case 2:
-        return '/candle-offset-2.gif';
-      case 3:
-        return '/candle-flip-offset-3.gif';
-      case 4:
-        return '/candle-offset-4.gif';
-      case 5:
-        return '/candle-flip-offset-5.gif';
-      case 6:
-        return '/candle-offset-6.gif';
-      case 7:
-      default:
-        return '/candle-flip-offset-7.gif';
-    }
-  };
-
-  // TODO: message 정보 등이 표시 되도록 개선
-  const drawCandle = (row: number, column: number, index: number) => {
-    if (divRef.current) {
-      divRef.current.insertAdjacentHTML(
-        'beforeend',
-        `<img src="${getRandomCandleImageSrc(
-          index
-        )}" alt="small candle" class="candle" style="top: ${
-          row * CANDLE_HEIGHT
-        }px; left: ${column * CANDLE_WIDTH}px;">`
-      );
-    }
-  };
-
-  // TODO: viewport가 변할 때마다 갱신
-  useMount(() => {
-    const screen = divRef.current;
-
-    if (screen) {
-      const screenWidth = screen.offsetWidth;
-      const screenHeight = screen.offsetHeight;
-      const columnColunt = Math.floor(screenWidth / CANDLE_WIDTH);
-      const rowColunt = Math.floor(screenHeight / CANDLE_HEIGHT);
-
-      const array = create2DArray(rowColunt, columnColunt);
-      assignTruthyValues(array, MOCK_VALUES.length);
-      const candles = addCoordinateFrom2DArray(MOCK_VALUES, array);
-      candles.map((candle, index) => {
-        drawCandle(candle.row, candle.column, index);
-      });
-    }
-  });
+  const [additionalCandles, setAdditionalCandles] = useState<Candle[]>([]);
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(event.target.value);
   };
 
-  const handleSubmit = () => {
-    // TODO: send message
+  const handleSubmit = async () => {
+    if (!user || message.length === 0) return;
+
+    const requestedMessage = message;
+    setMessage('');
+
+    try {
+      await postArrow({
+        message: requestedMessage,
+        userId: user._id,
+      });
+      setAdditionalCandles((candles) => [
+        ...candles,
+        { _id: createUniqueId(), message: requestedMessage },
+      ]);
+    } catch (error: any) {
+      const status = error?.response?.status;
+      if (status === 401) {
+        logout();
+        router.push('/expired');
+      } else {
+        enqueueSnackbar(formatMessage({ id: 'error.message.common' }), {
+          variant: 'error',
+        });
+      }
+    }
   };
 
   return (
@@ -112,32 +61,34 @@ const Arrows = () => {
       <Box height="100vh" display="flex" flexDirection="column">
         <Meta />
         <GlobalHeader colorMode="dark" />
-        <StyledBox
-          ref={divRef}
-          flexGrow={1}
-          sx={{ bgcolor: 'black', position: 'relative' }}
-        />
-        <Stack
-          direction="row"
-          p={1}
-          position="relative"
-          justifyContent="center"
-          sx={{ bgcolor: 'black' }}
-        >
-          <TextField
-            value={message}
-            onChange={handleChange}
-            size="small"
-            fullWidth
-            multiline
-            sx={{
-              maxWidth: 300,
-            }}
-          />
-          <Button size="small" onClick={handleSubmit}>
-            저장
-          </Button>
-        </Stack>
+        <Candles additionalCandles={additionalCandles} />
+        {user ? (
+          <Stack
+            direction="row"
+            p={1}
+            position="relative"
+            justifyContent="center"
+            sx={{ bgcolor: 'black' }}
+          >
+            <TextField
+              value={message}
+              onChange={handleChange}
+              size="small"
+              fullWidth
+              multiline
+              sx={{
+                maxWidth: 300,
+              }}
+            />
+            <Button
+              size="small"
+              disabled={message.length === 0}
+              onClick={handleSubmit}
+            >
+              저장
+            </Button>
+          </Stack>
+        ) : null}
       </Box>
     </ThemeProvider>
   );
