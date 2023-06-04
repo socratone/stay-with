ca4acee967dc354cd66580a19855b59ed45a9158
Kiwon Kim <kiwon@elicer.com>
2023-06-05 00:08:11 +0900

    feat: add display page

--- components/GlobalHeader/GlobalHeader.tsx
+++ components/GlobalHeader/GlobalHeader.tsx
@@ -23,6 +23,7 @@ import GlobalHeaderDrawer from './GlobalHeaderDrawer';
 import HeaderLink from './HeaderLink';
 
 type GlobalHeaderProps = {
+  // TODO: 삭제해도 괜찮은지 확인
   dark?: true;
 };
 
---
+++ pages/settings/display.tsx
@@ -0,0 +1,69 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import ImageToggleButtonGroup from 'components/ImageToggleButtonGroup/ImageToggleButtonGroup';
+import SettingsLayout from 'feature/SettingsLayout/SettingsLayout';
+import useFontSize from 'hooks/theme/useFontSize';
+
+const SettingsDisplay = () => {
+  const { fontSize, changeFontSize } = useFontSize();
+
+  const handleFontSizeChange = (fontSize: string) => {
+    changeFontSize(Number(fontSize));
+  };
+
+  return (
+    <SettingsLayout>
+      <Box py={1.5}>
+        <Stack spacing={1.5}>
+          <Box component="label">
+            <Typography
+              color="text.primary"
+              component="h2"
+              fontWeight={500}
+              variant="body1"
+              mb={0.5}
+            >
+              텍스트 크기
+            </Typography>
+            <ImageToggleButtonGroup
+              value={String(fontSize)}
+              onChange={handleFontSizeChange}
+              options={[
+                {
+                  image: {
+                    src: 'https://picsum.photos/id/1/200/300',
+                    alt: 'alt',
+                    width: 100,
+                    height: 100,
+                  },
+                  value: '24',
+                },
+                {
+                  image: {
+                    src: 'https://picsum.photos/id/1/200/300',
+                    alt: 'alt',
+                    width: 100,
+                    height: 100,
+                  },
+                  value: '20',
+                },
+                {
+                  image: {
+                    src: 'https://picsum.photos/id/1/200/300',
+                    alt: 'alt',
+                    width: 100,
+                    height: 100,
+                  },
+                  value: '16',
+                },
+              ]}
+            />
+          </Box>
+        </Stack>
+      </Box>
+    </SettingsLayout>
+  );
+};
+
+export default SettingsDisplay;
