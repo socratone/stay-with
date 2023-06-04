83ecd2b8b7fc922b855bed02a397c0c0c13a6202
Kiwon Kim <kiwon@elicer.com>
2023-06-04 18:58:45 +0900

    feat: add json value to persist utils

--- utils/persist.ts
+++ utils/persist.ts
@@ -9,6 +9,23 @@ export const getValue = <T>(key: string) => {
   return null;
 };
 
+export const setJsonValue = (key: string, value: object) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getJsonValue = <T>(key: string) => {
+  if (typeof window !== 'undefined') {
+    const stringifiedValue = localStorage.getItem(key);
+    if (stringifiedValue === null) return null;
+    try {
+      return JSON.parse(stringifiedValue) as T;
+    } catch {
+      return null;
+    }
+  }
+  return null;
+};
+
 export const removeValue = (key: string) => {
   localStorage.removeItem(key);
 };
