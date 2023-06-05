2d23e60fb8df110326b2c31b6ce9b9b77c4b98ae
Kiwon Kim <kiwon@elicer.com>
2023-06-06 01:45:51 +0900

    feat: add error, loading state to daily missa

--- components/DailyMissa/DailyMissa.tsx
+++ components/DailyMissa/DailyMissa.tsx
@@ -1,11 +1,14 @@
+import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
+import WaitingMessage from 'components/WaitingMessage/WaitingMessage';
 import { BIBLE_INTRO } from 'constants/bible';
 import useMissa from 'hooks/api/useMissa';
 
 import Word from './Word';
 
 const DailyMissa = () => {
-  const { data: missaData } = useMissa();
+  const { data: missaData, isLoading, isError } = useMissa();
 
   const parseToBible = (bibleInfo: string | null) => {
     if (!bibleInfo) return null;
@@ -20,15 +23,35 @@ const DailyMissa = () => {
 
   return (
     <Stack spacing={2} p={2} pb={12}>
-      {missaData?.words.map((word) => (
-        <Word
-          key={word.bibleInfo}
-          bible={parseToBible(word.bibleInfo)}
-          title={word.title}
-          bibleInfo={word.bibleInfo ?? ''}
-          contents={word.contents}
-        />
-      ))}
+      {isError ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="50vh"
+        >
+          <ErrorMessage />
+        </Box>
+      ) : isLoading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="50vh"
+        >
+          <WaitingMessage />
+        </Box>
+      ) : (
+        missaData?.words.map((word) => (
+          <Word
+            key={word.bibleInfo}
+            bible={parseToBible(word.bibleInfo)}
+            title={word.title}
+            bibleInfo={word.bibleInfo ?? ''}
+            contents={word.contents}
+          />
+        ))
+      )}
     </Stack>
   );
 };
