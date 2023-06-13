e40efeb7e6966fd5434499b1479ad3a5c900ab84
Kiwon Kim <kiwon@elicer.com>
2023-06-13 23:53:51 +0900

    feat: add arrow message length validation in client

--- schemas/arrow.ts
+++ schemas/arrow.ts
@@ -1,11 +1,11 @@
 import { InferType, object, string } from 'yup';
 
-import { ID_MAX_LENGTH } from './constants';
+import { ARROW_MESSAGE_MAX_LENGTH, ID_MAX_LENGTH } from './constants';
 
 export const arrowSchema = object({
   _id: string().required().max(ID_MAX_LENGTH),
   userId: string().required().max(ID_MAX_LENGTH),
-  message: string().required().max(500),
+  message: string().required().max(ARROW_MESSAGE_MAX_LENGTH),
 });
 
 export const arrowPostSchema = arrowSchema.omit(['_id']);
--- schemas/constants.ts
+++ schemas/constants.ts
@@ -2,3 +2,4 @@ export const ID_MAX_LENGTH = 50;
 export const COMMENT_MESSAGE_MAX_LENGTH = 500;
 export const BIBLE_CHAPTER_MAX_NUMBER = 1000;
 export const BIBLE_VERSE_MAX_NUMBER = 1000;
+export const ARROW_MESSAGE_MAX_LENGTH = 500;
