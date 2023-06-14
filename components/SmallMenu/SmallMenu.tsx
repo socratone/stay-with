4a4cbed3e532706fa6feabb624d465eae7cf10d4
Kiwon Kim <kiwon@elicer.com>
2023-06-15 02:37:28 +0900

    feat: add arrow to small menu

--- components/SmallMenu/SmallMenu.tsx
+++ components/SmallMenu/SmallMenu.tsx
@@ -1,20 +1,56 @@
 import { MenuProps, styled } from '@mui/material';
 import Menu from '@mui/material/Menu';
+import { useEffect, useState } from 'react';
 import { PRIMARY_SHADOW } from 'theme/shadows';
 
-const SmallMenu = styled((props: MenuProps) => (
-  <Menu
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
+const SmallMenu = styled((props: MenuProps) => {
+  const [offsetWidth, setOffsetWidth] = useState(34);
+
+  const htmlElement = props.anchorEl as HTMLElement;
+
+  useEffect(() => {
+    if (htmlElement?.offsetWidth) {
+      setOffsetWidth(htmlElement?.offsetWidth);
+    }
+  }, [htmlElement?.offsetWidth]);
+
+  return (
+    <Menu
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      PaperProps={{
+        sx: {
+          overflow: 'visible',
+        },
+      }}
+      MenuListProps={{
+        sx: {
+          overflow: 'unset',
+          '&::before': {
+            bgcolor: (theme) => theme.palette.background.paper,
+            backgroundImage:
+              'linear-gradient(rgba(255, 255, 255, 0.12), rgba(255, 255, 255, 0.12))',
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            width: 10,
+            height: 10,
+            top: 0,
+            right: offsetWidth / 2,
+            transform: 'translate(50%, -50%) rotate(45deg)',
+          },
+        },
+      }}
+      {...props}
+    />
+  );
+})(({ theme }) => ({
   '.MuiPaper-root': {
     color:
       theme.palette.mode === 'light'
