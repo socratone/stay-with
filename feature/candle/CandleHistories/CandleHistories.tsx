08058941cbad20f585a5d56fd610c64defdb94b2
Kiwon Kim <kiwon@elicer.com>
2023-06-26 01:12:57 +0900

    fix: message item container bgcolor

--- feature/candle/CandleHistories/CandleHistories.tsx
+++ feature/candle/CandleHistories/CandleHistories.tsx
@@ -86,33 +86,35 @@ const CandleHistories = () => {
 
   return (
     <>
-      <Container>
-        <Stack gap={1} bgcolor="#000">
-          {arrowsData?.arrows.map((arrow) => (
-            <MessageItem
-              key={arrow._id}
-              message={arrow.message}
-              profileImageUrl={arrow.user.imageUrl}
-              name={arrow.user.name}
-              createdAt={arrow.createdAt}
-              isMyself={arrow.userId === me?._id}
-              onEdit={() => handleEdit(arrow._id)}
-              onDelete={() => handleDelete(arrow._id)}
-            />
-          ))}
-          <Box display="flex" justifyContent="center">
-            {(arrowsCountData?.count ?? 0) <= ITEM_COUNT_PER_PAGE ? null : (
-              <Pagination
-                page={page}
-                onChange={(_, page) => handlePageChange(page)}
-                count={Math.ceil(
-                  Number(arrowsCountData?.count) / ITEM_COUNT_PER_PAGE
-                )}
+      <Box bgcolor="#000">
+        <Container>
+          <Stack gap={1}>
+            {arrowsData?.arrows.map((arrow) => (
+              <MessageItem
+                key={arrow._id}
+                message={arrow.message}
+                profileImageUrl={arrow.user.imageUrl}
+                name={arrow.user.name}
+                createdAt={arrow.createdAt}
+                isMyself={arrow.userId === me?._id}
+                onEdit={() => handleEdit(arrow._id)}
+                onDelete={() => handleDelete(arrow._id)}
               />
-            )}
-          </Box>
-        </Stack>
-      </Container>
+            ))}
+            <Box display="flex" justifyContent="center">
+              {(arrowsCountData?.count ?? 0) <= ITEM_COUNT_PER_PAGE ? null : (
+                <Pagination
+                  page={page}
+                  onChange={(_, page) => handlePageChange(page)}
+                  count={Math.ceil(
+                    Number(arrowsCountData?.count) / ITEM_COUNT_PER_PAGE
+                  )}
+                />
+              )}
+            </Box>
+          </Stack>
+        </Container>
+      </Box>
 
       <AlertDialog
         open={!!deleteDialog}
