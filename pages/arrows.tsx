3e7401452244f2740b918aba410c736f0a2b0f9a
Kiwon Kim <kiwon@elicer.com>
2023-06-28 21:46:46 +0900

    fix: prevent candles height changed

--- pages/arrows.tsx
+++ pages/arrows.tsx
@@ -6,19 +6,34 @@ import ArrowHistories from 'feature/candle/CandleHistories';
 import Candles from 'feature/candle/Candles';
 import Image from 'next/image';
 import crossImage from 'public/images/cross.webp';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Arrows = () => {
   const candlesContainerRef = useRef<HTMLDivElement>(null);
+  const [viewportHeight, setViewportHeight] = useState<number | null>(null);
 
+  // viewport height를 계산한 이후에 scroll 이동
   useEffect(() => {
-    if (candlesContainerRef.current) {
+    if (viewportHeight && candlesContainerRef.current) {
       window.scrollTo({
         top: candlesContainerRef.current.offsetHeight,
         behavior: 'smooth',
       });
     }
+  }, [viewportHeight]);
+
+  // viewport height를 계산
+  useEffect(() => {
+    const viewportHeight = window.innerHeight;
+    setViewportHeight(viewportHeight);
   }, []);
+
+  const getCandlesHeight = () => {
+    const initialHeight = '100vh';
+    const calculatedHeight = `${viewportHeight}px`;
+    const height = viewportHeight ? calculatedHeight : initialHeight;
+    return `calc(${height} - ${GLOBAL_HEADER_HEIGHT})`;
+  };
 
   return (
     <>
@@ -38,10 +53,7 @@ const Arrows = () => {
           style={{ objectFit: 'contain' }}
         />
       </Box>
-      <Box
-        ref={candlesContainerRef}
-        height={`calc(100vh - ${GLOBAL_HEADER_HEIGHT})`}
-      >
+      <Box ref={candlesContainerRef} height={getCandlesHeight()}>
         <Candles />
       </Box>
       <ArrowHistories />
