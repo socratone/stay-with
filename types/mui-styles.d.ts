33451de86c57e02f56358332ae095e9164179a5b
Kiwon Kim <kiwon@elicer.com>
2023-07-06 01:02:33 +0900

    feat: add custom typography token

---
+++ types/mui-styles.d.ts
@@ -0,0 +1,39 @@
+import { SimplePaletteColorOptions } from '@mui/material';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    paper?: Palette['primary'];
+    kakao: SimplePaletteColorOptions;
+  }
+
+  interface PaletteOptions {
+    paper?: PaletteOptions['primary'];
+    kakao: SimplePaletteColorOptions;
+  }
+
+  // markdown typography
+  interface TypographyVariants {
+    mh1: React.CSSProperties;
+    mh2: React.CSSProperties;
+    mh3: React.CSSProperties;
+    mp: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    mh1?: React.CSSProperties;
+    mh2?: React.CSSProperties;
+    mh3?: React.CSSProperties;
+    mp?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    mh1: true;
+    mh2: true;
+    mh3: true;
+    mp: true;
+  }
+}
