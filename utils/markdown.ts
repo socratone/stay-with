01a6d56f8e9a934cf0509c2337e3d519c8d8bd96
Kiwon Kim <kiwon@elicer.com>
2023-07-06 00:33:41 +0900

    feat: add markdown util

---
+++ utils/markdown.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import { remark } from 'remark';
+import html from 'remark-html';
+
+export const parseMarkdownToHtml = async (markdown: string) => {
+  const result = await remark().use(html).process(markdown);
+  return result.toString();
+};
+
+export const parseMarkdownFile = (fileName: string) => {
+  const fullPath = path.join(process.cwd(), fileName);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const { data, content } = matter(fileContents);
+
+  return {
+    metadata: data,
+    content,
+  };
+};
