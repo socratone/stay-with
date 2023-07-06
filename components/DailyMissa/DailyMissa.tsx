a7011f26ee6c03ceeeed2f4b8ffe8e22c9b816ae
Kiwon Kim <kiwon@elicer.com>
2023-07-07 02:18:10 +0900

    feat: add starting prayer dialog

--- components/DailyMissa/DailyMissa.tsx
+++ components/DailyMissa/DailyMissa.tsx
@@ -1,6 +1,7 @@
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
@@ -10,10 +11,13 @@ import useMissa from 'hooks/api/useMissa';
 import { useState } from 'react';
 
 import Loading from './Loading';
+import StartingPrayerDialog from './StartingPrayerDialog';
 import Word from './Word';
 
 const DailyMissa = () => {
+  const [startingPrayerOpened, setStartingPrayerOpened] = useState(false);
   const [offset, setOffset] = useState(0);
+
   const {
     data: missaData,
     isLoading,
@@ -31,6 +35,14 @@ const DailyMissa = () => {
     return null;
   };
 
+  const openStartingPrayer = () => {
+    setStartingPrayerOpened(true);
+  };
+
+  const closeStartingPrayer = () => {
+    setStartingPrayerOpened(false);
+  };
+
   const handlePrevious = () => {
     setOffset((offset) => offset - 1);
   };
@@ -40,53 +52,78 @@ const DailyMissa = () => {
   };
 
   return (
-    <Stack spacing={2}>
-      {isError ? (
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          height="50vh"
-        >
-          <ErrorMessage />
-        </Box>
-      ) : isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <Stack direction="row" justifyContent="flex-end">
-            <Typography color="text.primary" mr="auto">
-              {missaData.today}
-            </Typography>
-            <IconButton
-              size="small"
-              onClick={handlePrevious}
-              disabled={offset === -1}
-              sx={{ marginY: -1 }}
-            >
-              <KeyboardArrowLeftIcon />
-            </IconButton>
-            <IconButton
-              size="small"
-              onClick={handleNext}
-              disabled={offset === 1}
-              sx={{ marginY: -1 }}
-            >
-              <KeyboardArrowRightIcon />
-            </IconButton>
-          </Stack>
-          {missaData.words.map((word, index) => (
-            <Word
-              key={index}
-              bible={parseToBible(word.bibleInfo)}
-              title={word.title}
-              bibleInfo={word.bibleInfo ?? ''}
-              contents={word.contents}
-            />
-          ))}
-        </>
-      )}
-    </Stack>
+    <>
+      <Stack spacing={2}>
+        {isError ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="50vh"
+          >
+            <ErrorMessage />
+          </Box>
+        ) : isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <Stack direction="row" justifyContent="space-between" spacing={1}>
+              <Stack
+                direction="row"
+                alignItems="center"
+                gap={1}
+                flexWrap="wrap"
+              >
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  size="small"
+                  onClick={openStartingPrayer}
+                >
+                  시작 기도
+                </Button>
+                <Typography color="text.primary">{missaData.today}</Typography>
+              </Stack>
+              <Stack direction="row" alignItems="center">
+                <IconButton
+                  size="small"
+                  onClick={handlePrevious}
+                  disabled={offset === -1}
+                  sx={{ marginY: -1 }}
+                >
+                  <KeyboardArrowLeftIcon />
+                </IconButton>
+                <IconButton
+                  size="small"
+                  onClick={handleNext}
+                  disabled={offset === 1}
+                  sx={{ marginY: -1 }}
+                >
+                  <KeyboardArrowRightIcon />
+                </IconButton>
+              </Stack>
+            </Stack>
+
+            {missaData.words.map((word, index) => (
+              <Word
+                key={index}
+                bible={parseToBible(word.bibleInfo)}
+                title={word.title}
+                bibleInfo={word.bibleInfo ?? ''}
+                contents={word.contents}
+              />
+            ))}
+          </>
+        )}
+      </Stack>
+
+      {startingPrayerOpened ? (
+        <StartingPrayerDialog
+          open={startingPrayerOpened}
+          onClose={closeStartingPrayer}
+        />
+      ) : null}
+    </>
   );
 };
 
