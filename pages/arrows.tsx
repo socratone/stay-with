b294cfef2a48e5638d7179195ce2225bbcebfe0d
Kiwon Kim <kiwon@elicer.com>
2023-07-09 01:32:32 +0900

    feat: fit cross image layout

--- pages/arrows.tsx
+++ pages/arrows.tsx
@@ -4,13 +4,32 @@ import { GLOBAL_HEADER_HEIGHT } from 'components/GlobalHeader/constants';
 import Meta from 'components/Meta/Meta';
 import ArrowHistories from 'feature/candle/CandleHistories';
 import Candles from 'feature/candle/Candles';
+import useResizeListener from 'hooks/dom/useResizeListener';
+import useRemToPxNumber from 'hooks/theme/useRemToPxNumer';
 import Image from 'next/image';
 import crossImage from 'public/images/cross.webp';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+const CROSS_IMAGE_WIDTH = 720;
+const CROSS_IMAGE_HEIGHT = 960;
 
 const Arrows = () => {
+  const globalHeaderHeight = useRemToPxNumber(GLOBAL_HEADER_HEIGHT);
   const candlesContainerRef = useRef<HTMLDivElement>(null);
   const [viewportHeight, setViewportHeight] = useState<number | null>(null);
+
+  const calculateViewportHeight = useCallback(() => {
+    const viewportHeight = window.innerHeight;
+    setViewportHeight(viewportHeight);
+  }, []);
+
+  useResizeListener({
+    onResize: calculateViewportHeight,
+  });
+
+  useEffect(() => {
+    calculateViewportHeight();
+  }, [calculateViewportHeight]);
 
   // viewport height를 계산한 이후에 scroll 이동
   useEffect(() => {
@@ -22,12 +41,6 @@ const Arrows = () => {
     }
   }, [viewportHeight]);
 
-  // viewport height를 계산
-  useEffect(() => {
-    const viewportHeight = window.innerHeight;
-    setViewportHeight(viewportHeight);
-  }, []);
-
   const getCandlesHeight = () => {
     const initialHeight = '100vh';
     const calculatedHeight = `${viewportHeight}px`;
@@ -35,23 +48,32 @@ const Arrows = () => {
     return `calc(${height} - ${GLOBAL_HEADER_HEIGHT})`;
   };
 
+  // 이미지의 위쪽 부분이 잘리지 않도록
+  const getMaxWidth = () => {
+    if (viewportHeight) {
+      const elementHeight = viewportHeight - globalHeaderHeight;
+      return (elementHeight * CROSS_IMAGE_WIDTH) / CROSS_IMAGE_HEIGHT; // ratio
+    }
+  };
+
   return (
     <>
       <Meta />
       <GlobalHeader dark />
-      <Box
-        display="flex"
-        justifyContent="center"
-        position="relative"
-        height={`calc(100vh - ${GLOBAL_HEADER_HEIGHT})`}
-        bgcolor="#000"
-      >
-        <Image
-          src={crossImage}
-          alt="cross"
-          fill
-          style={{ objectFit: 'contain' }}
-        />
+      <Box height={`calc(100vh - ${GLOBAL_HEADER_HEIGHT})`} bgcolor="#000">
+        <Box
+          position="relative"
+          height="100%"
+          maxWidth={getMaxWidth()}
+          mx="auto"
+        >
+          <Image
+            src={crossImage}
+            alt="cross"
+            fill
+            style={{ objectFit: 'cover' }}
+          />
+        </Box>
       </Box>
       <Box ref={candlesContainerRef} height={getCandlesHeight()}>
         <Candles />
