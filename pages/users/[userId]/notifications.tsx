52b600d4e2505284121d60f006ff51a93aee3535
Kiwon Kim <kiwon@elicer.com>
2023-07-09 23:32:47 +0900

    feat: add loading and error case in notification

--- pages/users/[userId]/notifications.tsx
+++ pages/users/[userId]/notifications.tsx
@@ -1,17 +1,27 @@
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import GlobalHeader from 'components/GlobalHeader';
+import LoadingCircular from 'components/LoadingCircular/LoadingCircular';
 import Meta from 'components/Meta';
 import NotificationItem from 'components/NotificationItem/NotificationItem';
 import useNotifications from 'hooks/api/useNotifications';
 import useAuth from 'hooks/auth/useAuth';
+import { FormattedMessage } from 'react-intl';
 
 const Notifications = () => {
   const { user } = useAuth();
 
-  const { data: notificationsData } = useNotifications({
+  const {
+    data: notificationsData,
+    isLoading,
+    isError,
+  } = useNotifications({
     userId: user?._id,
   });
+
+  const isEmpty = !isLoading && notificationsData?.notifications.length === 0;
 
   return (
     <>
@@ -19,17 +29,31 @@ const Notifications = () => {
       <GlobalHeader backButton />
       <Container>
         <Stack gap={1} py={2}>
-          {notificationsData?.notifications.map((notification) => (
-            <NotificationItem
-              key={notification._id}
-              id={notification._id}
-              type={notification.type}
-              user={notification.notifier}
-              message={notification.message}
-              isNew={notification.isNew}
-              lexioDivinaId={notification.lexioDivinaId}
-            />
-          ))}
+          {isLoading ? (
+            <LoadingCircular />
+          ) : isError ? (
+            <Box display="flex" justifyContent="center">
+              <Typography color="text.primary">
+                <FormattedMessage id="error.message.common" />
+              </Typography>
+            </Box>
+          ) : isEmpty ? (
+            <Box display="flex" justifyContent="center">
+              <Typography color="text.primary">알림이 없어요 🔔</Typography>
+            </Box>
+          ) : (
+            notificationsData?.notifications.map((notification) => (
+              <NotificationItem
+                key={notification._id}
+                id={notification._id}
+                type={notification.type}
+                user={notification.notifier}
+                message={notification.message}
+                isNew={notification.isNew}
+                lexioDivinaId={notification.lexioDivinaId}
+              />
+            ))
+          )}
         </Stack>
       </Container>
     </>
