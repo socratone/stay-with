d1915c349abe46f4b10f4581046a83e7d067cd17
Kiwon Kim <kiwon@elicer.com>
2023-07-13 21:57:09 +0900

    feat: add pagination to notifications

--- pages/users/[userId]/notifications.tsx
+++ pages/users/[userId]/notifications.tsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import GlobalHeader from 'components/GlobalHeader';
@@ -7,11 +8,17 @@ import LoadingCircular from 'components/LoadingCircular/LoadingCircular';
 import Meta from 'components/Meta';
 import NotificationItem from 'components/NotificationItem/NotificationItem';
 import useNotifications from 'hooks/api/useNotifications';
+import useNotificationsCount from 'hooks/api/useNotificationsCount';
 import useAuth from 'hooks/auth/useAuth';
+import { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
+
+const ITEM_COUNT_PER_PAGE = 20;
 
 const Notifications = () => {
   const { user } = useAuth();
+
+  const [page, setPage] = useState(1);
 
   const {
     data: notificationsData,
@@ -19,9 +26,20 @@ const Notifications = () => {
     isError,
   } = useNotifications({
     userId: user?._id,
+    skip: (page - 1) * ITEM_COUNT_PER_PAGE,
+    limit: ITEM_COUNT_PER_PAGE,
+  });
+
+  const { data: notificationsCountData } = useNotificationsCount({
+    userId: user?._id,
   });
 
   const isEmpty = !isLoading && notificationsData?.notifications.length === 0;
+
+  const handlePageChange = (page: number) => {
+    window.scrollTo({ top: 0 });
+    setPage(page);
+  };
 
   return (
     <>
@@ -55,6 +73,18 @@ const Notifications = () => {
             ))
           )}
         </Stack>
+        <Box display="flex" justifyContent="center">
+          {(notificationsCountData?.count ?? 0) <=
+          ITEM_COUNT_PER_PAGE ? null : (
+            <Pagination
+              page={page}
+              onChange={(_, page) => handlePageChange(page)}
+              count={Math.ceil(
+                Number(notificationsCountData?.count) / ITEM_COUNT_PER_PAGE
+              )}
+            />
+          )}
+        </Box>
       </Container>
     </>
   );
