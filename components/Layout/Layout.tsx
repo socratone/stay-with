cb6609f81cb55f3c817b6772155b1ae2e68eb961
Kiwon Kim <kiwon@elicer.com>
2023-07-26 00:38:17 +0900

    rename: to back button pathnames

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,10 +2,7 @@ import GlobalHeader from 'components/GlobalHeader';
 import Meta from 'components/Meta';
 import { useRouter } from 'next/router';
 
-import {
-  DARK_HEADER_PATHNAMES,
-  HEADER_BACK_BUTTON_PATHNAMES,
-} from './constants';
+import { BACK_BUTTON_PATHNAMES, DARK_HEADER_PATHNAMES } from './constants';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -18,7 +15,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     (pathname) => pathname === router.pathname
   );
 
-  const isBackButton = HEADER_BACK_BUTTON_PATHNAMES.some(
+  const isBackButton = BACK_BUTTON_PATHNAMES.some(
     (pathname) => pathname === router.pathname
   );
 
--- components/Layout/constants.ts
+++ components/Layout/constants.ts
@@ -1,6 +1,6 @@
 export const DARK_HEADER_PATHNAMES = ['/arrows'];
 
-export const HEADER_BACK_BUTTON_PATHNAMES = [
+export const BACK_BUTTON_PATHNAMES = [
   '/lexio-divinas/create',
   '/lexio-divinas/[lexioId]/edit',
   '/users/[userId]/notifications',
