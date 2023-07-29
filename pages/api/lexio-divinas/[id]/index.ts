188dbe1dd85183a871133a082a01f96ae1740989
Kiwon Kim <kiwon@elicer.com>
2023-07-29 16:11:20 +0900

    feat: add 404 to lexio divinas detail api when _id hash invalid

--- pages/api/lexio-divinas/[id]/index.ts
+++ pages/api/lexio-divinas/[id]/index.ts
@@ -78,7 +78,12 @@ const handler = async (
 
       db.close();
       return res.status(200).json({ lexioDivina });
-    } catch (error) {
+    } catch (error: any) {
+      // _id의 length가 규격에 안 맞는 경우
+      if (error?.name === 'BSONError') {
+        return res.status(404).json({ error: { message: 'Not found.' } });
+      }
+
       sendServerError(res, error);
     }
   }
