1f7b7a6c23cdbfd1be1c2227611f9c36e4ff669b
Kiwon Kim <kiwon@elicer.com>
2023-07-31 23:57:39 +0900

    feat: prevent initial scrolling position in iphone

--- pages/arrows.tsx
+++ pages/arrows.tsx
@@ -2,33 +2,30 @@ import Box from '@mui/material/Box';
 import { GLOBAL_HEADER_HEIGHT } from 'components/GlobalHeader/constants';
 import ArrowHistories from 'feature/candle/CandleHistories';
 import Candles from 'feature/candle/Candles';
+import useRemToPxNumber from 'hooks/theme/useRemToPxNumer';
 import Image from 'next/image';
 import crossImage from 'public/images/cross.webp';
 import { useEffect, useRef, useState } from 'react';
 
 const Arrows = () => {
+  const globalHeaderHeight = useRemToPxNumber(GLOBAL_HEADER_HEIGHT);
   const candlesContainerRef = useRef<HTMLDivElement>(null);
   const [viewportHeight, setViewportHeight] = useState<number | null>(null);
 
-  // viewport height를 계산한 이후에 scroll 이동
-  useEffect(() => {
-    if (viewportHeight && candlesContainerRef.current) {
-      window.scrollTo({
-        top: candlesContainerRef.current.offsetHeight,
-        behavior: 'smooth',
-      });
-    }
-  }, [viewportHeight]);
-
-  // viewport height를 계산
   useEffect(() => {
     const viewportHeight = window.innerHeight;
     setViewportHeight(viewportHeight);
-  }, []);
+
+    window.scrollTo({
+      top: viewportHeight - globalHeaderHeight,
+      behavior: 'smooth',
+    });
+  }, [globalHeaderHeight]);
 
   const getCandlesHeight = () => {
     const initialHeight = '100vh';
     const calculatedHeight = `${viewportHeight}px`;
+    // candles height가 달라지는 이슈 방지
     const height = viewportHeight ? calculatedHeight : initialHeight;
     return `calc(${height} - ${GLOBAL_HEADER_HEIGHT})`;
   };
