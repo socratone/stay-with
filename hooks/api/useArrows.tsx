d75831adfd45c3e5969751f9159fc11218d10b2a
Kiwon Kim <kiwon@elicer.com>
2023-08-02 01:03:03 +0900

    fix: arrows hooks params type

--- hooks/api/useArrows.tsx
+++ hooks/api/useArrows.tsx
@@ -1,14 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { getArrows, GetLexioDivinasParams } from 'helpers/axios';
+import { getArrows, GetArrowsParams } from 'helpers/axios';
 
 type UseArrowsOptions = { enabled?: boolean };
 
 export const ARROWS_QUERY_KEY = 'arrows';
 
-const useArrows = (
-  params: GetLexioDivinasParams,
-  options?: UseArrowsOptions
-) => {
+const useArrows = (params: GetArrowsParams, options?: UseArrowsOptions) => {
   return useQuery({
     queryKey: [ARROWS_QUERY_KEY, params],
     queryFn: () => getArrows(params),
