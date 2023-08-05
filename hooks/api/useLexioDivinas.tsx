98da855858a5539f4867dbffde93fee4047e6039
Kiwon Kim <kiwon@elicer.com>
2023-08-05 18:31:03 +0900

    feat: add enabled option to lexio divina data hooks

--- hooks/api/useLexioDivinas.tsx
+++ hooks/api/useLexioDivinas.tsx
@@ -1,13 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import { getLexioDivinas, GetLexioDivinasParams } from 'helpers/axios';
 
+type UseLexioDivinasOptions = {
+  enabled?: boolean;
+};
+
 export const LEXIO_DIVINAS_QUERY_KEY = 'lexio-divinas';
 
-const useLexioDivinas = (params?: GetLexioDivinasParams) => {
+const useLexioDivinas = (
+  params?: GetLexioDivinasParams,
+  options?: UseLexioDivinasOptions
+) => {
   return useQuery({
     queryKey: [LEXIO_DIVINAS_QUERY_KEY, params],
     queryFn: () => getLexioDivinas(params),
     keepPreviousData: true,
+    enabled: options?.enabled,
   });
 };
 
