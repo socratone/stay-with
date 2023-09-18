0478dbce5f38e740fb83dd923e520f66a0118b39
Kiwon Kim <kiwon@elicer.com>
2023-09-18 23:15:27 +0900

    fix: activate kakao login loading message

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,45 +1,11 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import ErrorMessage from 'components/ErrorMessage';
 import { GLOBAL_HEADER_HEIGHT } from 'components/GlobalHeader/constants';
 import KakaoLoginButton from 'components/KakaoLoginButton';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-
-type Error = {
-  message: string;
-};
+import { useState } from 'react';
 
 const Login = () => {
   const [isRequested, setIsRequested] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    let timer: NodeJS.Timer;
-
-    if (isRequested) {
-      timer = setTimeout(() => {
-        setError({ message: '카카오 로그인 페이지에 이동할 수 없습니다 😂' });
-      }, 10000);
-    }
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [isRequested]);
-
-  if (error) {
-    return (
-      <Box
-        height={`calc(100vh - ${GLOBAL_HEADER_HEIGHT})`}
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <ErrorMessage content={error.message} />
-      </Box>
-    );
-  }
 
   return (
     <Box
@@ -61,7 +27,10 @@ const Login = () => {
             ? '카카오 로그인을 시도중입니다 🥰'
             : '서비스를 이용하려면 카카오 계정이 필요해요 🙂'}
         </Typography>
-        <KakaoLoginButton disabled={isRequested} />
+        <KakaoLoginButton
+          disabled={isRequested}
+          onClick={() => setIsRequested(true)}
+        />
       </Box>
     </Box>
   );
